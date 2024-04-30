15249b0e5ab4af30138b27c44b995e34e4aee24e
izulx1 <166265491+izulx1@users.noreply.github.com>
2024-04-30 23:31:55 +0700

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+os=$(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g')
+os2=$(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')
+
+echo "$(wget -qO- ipinfo.io/ip)" > /root/.myip
+MYIP=$(cat /root/.myip)
+
 clear
 mkdir -p /etc/xray
 GITHUB_CMD="https://github.com/izulx1/ssh/raw/master/files/"
 function add_domain() {
-    read -p "Input Domain :  " domain
-    if [[ ${domain} ]]; then
-        echo $domain >/etc/xray/domain
-    else
-        echo -e " Please input your Domain"
-        add_domain
-    fi
+read -p "Input Domain :  " domain
+if [[ ${domain} ]]; then
+echo $domain >/etc/xray/domain
+else
+echo -e " Please input your Domain"
+add_domain
+fi
 }
+
 add_domain
 apt update -y
 apt install sudo -y
@@ -22,68 +30,67 @@ sudo apt-get remove --purge ufw firewalld -y
 apt install -y --no-install-recommends software-properties-common
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
-apt install -y python speedtest-cli jq iptables iptables-persistent netfilter-persistent net-tools socat cron dropbear squid neofetch
+apt install -y speedtest-cli jq iptables iptables-persistent netfilter-persistent net-tools socat cron dropbear squid neofetch
 
-if [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "ubuntu" ]]; then
-    echo "Setup Dependencies $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-    sudo apt update -y
-    apt-get install --no-install-recommends software-properties-common
-    add-apt-repository ppa:vbernat/haproxy-2.0 -y
-    apt-get -y install haproxy=2.0.\*
-elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "debian" ]]; then
-    echo "Setup Dependencies For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-    curl https://haproxy.debian.net/bernat.debian.org.gpg |
-        gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
-    echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
-        http://haproxy.debian.net buster-backports-1.8 main \
-        >/etc/apt/sources.list.d/haproxy.list
-    sudo apt-get update
-    apt-get -y install haproxy=1.8.\*
+if [[ $os == "ubuntu" ]]; then
+echo "Setup Dependencies $os2"
+sudo apt update -y
+apt-get install --no-install-recommends software-properties-common
+add-apt-repository ppa:vbernat/haproxy-2.0 -y
+apt-get -y install haproxy=2.0.\*
+elif [[ $os == "debian" ]]; then
+echo "Setup Dependencies For OS Is $os2"
+curl https://haproxy.debian.net/bernat.debian.org.gpg |
+gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
+echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
+http://haproxy.debian.net buster-backports-1.8 main \
+>/etc/apt/sources.list.d/haproxy.list
+sudo apt-get update
+apt-get -y install haproxy=1.8.\*
 else
-    echo -e " Your OS Is Not Supported ($(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g') )"
-    exit 1
+rcho -e " Your OS Is Not Supported ($os2 )"
+exit 1
 fi
+
 source <(curl -sL ${GITHUB_CMD}bbr)
-MYIP=$(wget -qO- ipinfo.io/ip)
-if [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "ubuntu" ]]; then
-    echo "Setup nginx For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-    rm -f /etc/apt/sources.list.d/nginx.list
-    apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
-    curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor |
-        tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-    echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-    http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" |
-        tee /etc/apt/sources.list.d/nginx.list
-    echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" |
-        tee /etc/apt/preferences.d/99nginx
 
-    apt update -y
-    apt install -y nginx
-elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "debian" ]]; then
-    echo "Setup nginx For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-    INS="apt install -y"
-
-    rm -f /etc/apt/sources.list.d/nginx.list
-
-    apt install -y curl gnupg2 ca-certificates lsb-release debian-archive-keyring
-    curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor |
-        tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-    echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-    http://nginx.org/packages/debian $(lsb_release -cs) nginx" |
-        tee /etc/apt/sources.list.d/nginx.list
-    echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" |
-        tee /etc/apt/preferences.d/99nginx
-
-    apt update -y
-    apt install -y nginx
+if [[ $os == "ubuntu" ]]; then
+echo "Setup nginx For OS Is $os2"
+rm -f /etc/apt/sources.list.d/nginx.list
+apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor |
+tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" |
+tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" |
+tee /etc/apt/preferences.d/99nginx
+apt update -y
+apt install -y nginx
+elif [[ $os == "debian" ]]; then
+echo "Setup nginx For OS Is $os2 "
+INS="apt install -y"
+rm -f /etc/apt/sources.list.d/nginx.list
+apt install -y curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor |
+tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/debian $(lsb_release -cs) nginx" |
+tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" |
+tee /etc/apt/preferences.d/99nginx
+apt update -y
+apt install -y nginx
 else
-    echo -e "Your OS Is Not Supported ($(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g') )"
-    exit 1
+echo -e "Your OS Is Not Supported ($os2 )"
+exit 1
 fi
+
 cat >/root/.profile <<END
 # ~/.profile: executed by Bourne-compatible login shells.
 if [ "$BASH" ]; then
   if [ -f ~/.bashrc ]; then
+
     . ~/.bashrc
   fi
 fi
@@ -94,6 +101,65 @@ neofetch
 echo "Silahkan Ketik menu Untuk Melihat daftar Perintah"
 END
 cd
+cat> /usr/bin/clearlog <<-END
+#!/bin/bash
+systemctl restart udp
+data=(`find /var/log/ -name *.log`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name *.err`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name mail.*`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+echo > /var/log/syslog
+echo > /var/log/btmp
+echo > /var/log/messages
+echo > /var/log/debug
+echo 1 > /proc/sys/vm/drop_caches
+END
+
+cat> /usr/bin/xp <<-END
+##------ Auto Remove SSH
+hariini=$(date +%d-%m-%Y)
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d >/tmp/expirelist.txt
+totalaccounts=$(cat /tmp/expirelist.txt | wc -l)
+for ((i = 1; i <= $totalaccounts; i++)); do
+    tuserval=$(head -n $i /tmp/expirelist.txt | tail -n 1)
+    username=$(echo $tuserval | cut -f1 -d:)
+    userexp=$(echo $tuserval | cut -f2 -d:)
+    userexpireinseconds=$(($userexp * 86400))
+    tglexp=$(date -d @$userexpireinseconds)
+    tgl=$(echo $tglexp | awk -F" " '{print $3}')
+    while [ ${#tgl} -lt 2 ]; do
+        tgl="0"$tgl
+    done
+    while [ ${#username} -lt 15 ]; do
+        username=$username" "
+    done
+    bulantahun=$(echo $tglexp | awk -F" " '{print $2,$6}')
+    todaystime=$(date +%s)
+    if [ $userexpireinseconds -ge $todaystime ]; then
+        :
+    else
+        userdel --force $username
+        systemctl reload ssh
+        systemctl restart ws
+    fi
+done
+echo -e " Successfully Delete User Expired"
+END
+
 wget -q -O /usr/bin/menu "${GITHUB_CMD}menu.sh"
 wget -q -O /etc/squid/squid.conf "${GITHUB_CMD}squid.conf"
 wget -q -O /etc/default/dropbear "${GITHUB_CMD}dropbear"
@@ -114,10 +180,12 @@ wget -q -O /etc/udp/udp-custom "https://github.com/izulx1/ssh/raw/master/udp/udp
 wget -q -O /etc/udp/config.json "https://github.com/izulx1/ssh/raw/master/udp/config.json"
 wget -q -O /etc/systemd/system/udp.service "https://github.com/izulx1/ssh/raw/master/udp/udp.service"
 sed -i "s/xxx/${MYIP}/g" /etc/squid/squid.conf
-chmod +x /usr/sbin/xdvpn
-chmod +x /usr/bin/ws
+chmod 777 /usr/sbin/xdvpn
+chmod 777 /usr/bin/ws
 chmod 777 /etc/udp/config.json
-chmod +x /etc/udp/udp-custom
+chmod 777 /etc/udp/udp-custom
+chmod 777 /usr/bin/clearlog
+chmod 777 /usr/bin/xp
 rm -rf /lib/systemd/system/haproxy.service
 cat >/etc/systemd/system/haproxy.service <<-END
 [Unit]
@@ -142,7 +210,7 @@ Type=notify
 [Install]
 WantedBy=multi-user.target
 END
-chmod +x /usr/bin/menu
+chmod 777 /usr/bin/menu
 mkdir /root/.acme.sh
 systemctl daemon-reload
 systemctl stop haproxy
@@ -157,9 +225,30 @@ cat /etc/xray/xray.crt /etc/xray/xray.key | tee /etc/haproxy/hap.pem
 
 # Monitoring Gotop
 gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-    gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
-    curl -sL "$gotop_link" -o /tmp/gotop.deb
-    dpkg -i /tmp/gotop.deb >/dev/null 2>&1
+gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
+curl -sL "$gotop_link" -o /tmp/gotop.deb
+dpkg -i /tmp/gotop.deb >/dev/null 2>&1
+
+# Auto Clear Log Setiap 120 Menit
+cat >/etc/cron.d/clearlog <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/120 * * * * root clearlog
+END
+
+# Auto Delete User SSH Expired
+cat >/etc/cron.d/del_exp <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+2 0 * * * root xp
+END
+
+# Auto Reboot VPS Jam 3 Pagi
+cat >/etc/cron.d/del_exp <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+* 3 * * * root reboot
+END
 
 # Swap 1GB
 dd if=/dev/zero of=/swapfile bs=1024 count=1048576
@@ -167,16 +256,16 @@ mkswap /swapfile
 chown root:root /swapfile
 chmod 0600 /swapfile >/dev/null 2>&1
 swapon /swapfile >/dev/null 2>&1
-sed -i '$ i\/swapfile      swap swap   defaults    0 0' /etc/fstab
+sed -i '$ i\/swapfile  swap swap   defaults 0 0' /etc/fstab
 
 systemctl daemon-reload
 systemctl enable client server
 systemctl enable netfilter-persistent
-systemctl enable ws haproxy udp
+systemctl enable ws haproxy udp cron
 systemctl start client server haproxy
 systemctl start netfilter-persistent
 systemctl restart client server
-systemctl restart ws cron dropbear haproxy rc-local sshd nginx
+systemctl restart ws cron dropbear haproxy rc-local sshd nginx cron
 systemctl restart netfilter-persistent
 clear
 neofetch
